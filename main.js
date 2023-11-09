fbcf6cff6310bd80bac3cadaba46d40dfaec48e4
natannsoares <119411819+natannsoares@users.noreply.github.com>
2023-11-09 20:53:16 -0300

    teste

--- main.js
+++ main.js
@@ -1,29 +1,17 @@
-// Criada a função para tocar o som e fazer a busca pelo ID do som
-function tocaSom(idElementoAudio) {
-    // Pausa qualquer áudio em execução
-    const audioEmExecucao = document.querySelector(".audio-em-execucao");
-    if (audioEmExecucao) {
-        audioEmExecucao.pause();
-        audioEmExecucao.classList.remove("audio-em-execucao");
-    }
-
-    // Toca o novo áudio
-    const novoAudio = document.querySelector(idElementoAudio);
-    novoAudio.play();
-    novoAudio.classList.add("audio-em-execucao");
+//criado a função para tocar o som e feito a busca id do som
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
-// Constante das teclas buscando todos os botões da classe
+//constante das teclas com busca de todos os botões da classe
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-// Criado o contador e o laço de repetição while tocando com o click
-let contador = 0;
-while (contador < listaDeTeclas.length) {
-    const efeito = listaDeTeclas[contador].classList[1];
-    const idAudio = "#som_" + efeito;
-    listaDeTeclas[contador].onclick = function () {
-        tocaSom(idAudio);
+//criado o contador e o laço de repetição while tocando com o click
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+    const tecla = listaDeTeclas[contador];
+    const efeito = tecla.classList[1];
+    const idAudio = '#som_${efeito}';
+    tecla.onclick = function(){
+        tocaSom(idAudio)
     }
-    contador = contador + 1;
-    console.log(contador);
 }
