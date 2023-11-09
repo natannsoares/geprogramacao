a98b7fb0da10dacb30058e49d14addcd8140d15f
natannsoares <119411819+natannsoares@users.noreply.github.com>
2023-11-09 20:58:13 -0300

    som

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-//criado a função para tocar o som e feito a busca id do som
 function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
 
-//constante das teclas com busca de todos os botões da classe
+// Constante das teclas buscando todos os botões da classe
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-//criado o contador e o laço de repetição while tocando com o click
-for(let contador = 0; contador < listaDeTeclas.length; contador++){
+// Loop for...of para associar a função de tocar som ao clique de cada tecla
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const efeito = tecla.classList[1];
-    const idAudio = '#som_${efeito}';
-    tecla.onclick = function(){
-        tocaSom(idAudio)
-    }
+    const idAudio = `#som_${efeito}`;  // Usando crases (`) para criar template string
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    };
 }
