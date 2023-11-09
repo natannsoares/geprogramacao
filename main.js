52df4aad2eee08305b0c02ea6c08e5b6010a706c
natannsoares <119411819+natannsoares@users.noreply.github.com>
2023-11-09 12:34:32 -0300

    teste2

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
-//criado a função para tocar o som e feito a busca id do som
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+// Criada a função para tocar o som e fazer a busca pelo ID do som
+function tocaSom(idElementoAudio) {
+    // Pausa qualquer áudio em execução
+    const audioEmExecucao = document.querySelector(".audio-em-execucao");
+    if (audioEmExecucao) {
+        audioEmExecucao.pause();
+        audioEmExecucao.classList.remove("audio-em-execucao");
+    }
+
+    // Toca o novo áudio
+    const novoAudio = document.querySelector(idElementoAudio);
+    novoAudio.play();
+    novoAudio.classList.add("audio-em-execucao");
 }
 
-//constante das teclas com busca de todos os botões da classe
+// Constante das teclas buscando todos os botões da classe
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-//criado o contador e o laço de repetição while tocando com o click
+// Criado o contador e o laço de repetição while tocando com o click
 let contador = 0;
 while (contador < listaDeTeclas.length) {
     const efeito = listaDeTeclas[contador].classList[1];
     const idAudio = "#som_" + efeito;
-    listaDeTeclas[contador].onclick = function (){
+    listaDeTeclas[contador].onclick = function () {
         tocaSom(idAudio);
     }
     contador = contador + 1;
