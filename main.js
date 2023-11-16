28842cbd75f20fb5af21c85d14b93f2ec357bcfa
natannsoares <119411819+natannsoares@users.noreply.github.com>
2023-11-16 20:49:34 -0300

    função

--- main.js
+++ main.js
@@ -9,8 +9,17 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const efeito = tecla.classList[1];
-    const idAudio = `#som_${efeito}`;  // Usando crases (`) para criar template string
+    const idAudio = `#som_${efeito}`; 
     tecla.onclick = function() {
         tocaSom(idAudio);
     };
+    tecla.onkeydown = function(evento){
+        if(evento.code != 'Tab'){ 
+        tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
+    }
 }
+
